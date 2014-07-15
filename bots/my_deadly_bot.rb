50ba7fbbb067214fe5a1729da8380d8657d8ae92
Jason Watson <jswatson0@gmail.com>
2014-07-15 15:55:39 -0500

    initial commit

--- bots/my_deadly_bot.rb
+++ bots/my_deadly_bot.rb
@@ -2,16 +2,49 @@ class MyDeadlyBot < RTanque::Bot::Brain
   NAME = 'my_deadly_bot'
   include RTanque::Bot::BrainHelper
 
+  TURRET_FIRE_RANGE = RTanque::Heading::ONE_DEGREE * 10
+
   def tick!
-    ## main logic goes here
-    
-    # use self.sensors to detect things
-    # See http://rubydoc.info/github/awilliams/RTanque/master/RTanque/Bot/Sensors
-    
-    # use self.command to control tank
-    # See http://rubydoc.info/github/awilliams/RTanque/master/RTanque/Bot/Command
-    
-    # self.arena contains the dimensions of the arena
-    # See http://rubydoc.info/github/awilliams/RTanque/master/frames/RTanque/Arena
+    @desired_heading ||= nil
+
+    if (lock = self.get_radar_lock)
+      self.destroy_lock(lock)
+      @desired_heading = nil
+    else
+      self.seek_lock
+    end
+  end
+
+  def destroy_lock(reflection)
+    command.heading = reflection.heading
+    command.radar_heading = reflection.heading
+    command.turret_heading = reflection.heading
+    command.speed = reflection.distance > 200 ? MAX_BOT_SPEED : MAX_BOT_SPEED / 2.0
+    if (reflection.heading.delta(sensors.turret_heading)).abs < TURRET_FIRE_RANGE
+      command.fire(reflection.distance > 200 ? MAX_FIRE_POWER : MIN_FIRE_POWER)
+    end
+  end
+
+  def seek_lock
+    if sensors.position.on_wall?
+      @desired_heading = sensors.heading + RTanque::Heading::HALF_ANGLE
+    end
+    command.radar_heading = sensors.radar_heading + MAX_RADAR_ROTATION
+    command.speed = 1
+    if @desired_heading
+      command.heading = @desired_heading
+      command.turret_heading = @desired_heading
+    end
+  end
+
+  def get_radar_lock
+    @locked_on ||= nil
+    lock = if @locked_on
+             sensors.radar.find { |reflection| reflection.name == @locked_on } || sensors.radar.first
+           else
+             sensors.radar.first
+           end
+    @locked_on = lock.name if lock
+    lock
   end
 end
